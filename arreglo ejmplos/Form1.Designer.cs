503ec0d7b003290b4a69b60673c5de5ffd82a356
Gaston2002 <gastonmoriconi15@gmail.com>
2022-08-01 10:25:53 -0300

    Agregar archivos de proyecto.

---
+++ arreglo ejmplos/Form1.Designer.cs
@@ -0,0 +1,113 @@
+﻿namespace arreglo_ejmplos
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btMostar = new System.Windows.Forms.Button();
+            this.lblLista = new System.Windows.Forms.Label();
+            this.txtNombre = new System.Windows.Forms.TextBox();
+            this.btCargar = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // btMostar
+            // 
+            this.btMostar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btMostar.Location = new System.Drawing.Point(173, 79);
+            this.btMostar.Name = "btMostar";
+            this.btMostar.Size = new System.Drawing.Size(75, 34);
+            this.btMostar.TabIndex = 9;
+            this.btMostar.Text = "Mostar";
+            this.btMostar.UseVisualStyleBackColor = true;
+            this.btMostar.Click += new System.EventHandler(this.btMostar_Click);
+            // 
+            // lblLista
+            // 
+            this.lblLista.AutoSize = true;
+            this.lblLista.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblLista.Location = new System.Drawing.Point(12, 79);
+            this.lblLista.Name = "lblLista";
+            this.lblLista.Size = new System.Drawing.Size(43, 20);
+            this.lblLista.TabIndex = 8;
+            this.lblLista.Text = "Lista";
+            // 
+            // txtNombre
+            // 
+            this.txtNombre.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNombre.Location = new System.Drawing.Point(12, 43);
+            this.txtNombre.Name = "txtNombre";
+            this.txtNombre.Size = new System.Drawing.Size(144, 26);
+            this.txtNombre.TabIndex = 7;
+            // 
+            // btCargar
+            // 
+            this.btCargar.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btCargar.Location = new System.Drawing.Point(173, 39);
+            this.btCargar.Name = "btCargar";
+            this.btCargar.Size = new System.Drawing.Size(75, 34);
+            this.btCargar.TabIndex = 6;
+            this.btCargar.Text = "Cargar";
+            this.btCargar.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 20);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(51, 20);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "label1";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btMostar);
+            this.Controls.Add(this.lblLista);
+            this.Controls.Add(this.txtNombre);
+            this.Controls.Add(this.btCargar);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btMostar;
+        private System.Windows.Forms.Label lblLista;
+        private System.Windows.Forms.TextBox txtNombre;
+        private System.Windows.Forms.Button btCargar;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ arreglo ejmplos/Form1.cs
@@ -0,0 +1,38 @@
+﻿using arreglo_ejmplos.clases;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arreglo_ejmplos
+{
+    public partial class Form1 : Form
+    {
+        public persona[] personas { get; set; } = new persona[1];
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btCargar_Click(object sender, EventArgs e, persona personas)
+        {
+            persona persona = new persona();
+            personas.nombres = txtNombre.Text;
+            //persona [0] = personas;
+        }
+
+        private void btMostar_Click(object sender, EventArgs e)
+        {
+            lblLista.Text = "lista: \r\n";
+            foreach (persona item in personas)
+            {
+                lblLista.Text = lblLista.Text + item.nombres + "\r\n";
+            }
+        }
+    }
+}
